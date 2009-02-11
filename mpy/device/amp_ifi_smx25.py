54c4f6afd0e7493820f8efe24f108983f7104a50
Hans Georg KrauthÃ¤user <hgk@ieee.org>
2009-02-11 16:31:11 +0100

    debug session finished

--- mpy/device/amp_ifi_smx25.py
+++ mpy/device/amp_ifi_smx25.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import visa
+import time
 from mpy.device.amplifier import AMPLIFIER
 
 class SMX25(AMPLIFIER):
@@ -20,21 +21,36 @@ class SMX25(AMPLIFIER):
         self.write('ZA')
         return self.error
 
-    def SetFreq(self, freq):
-        AMPLIFIER.SetFreq(self,freq)
+    def _getstat(self):
         dct=self.query('ST', r'(?P<st>.*)')
         B1=int(dct['st'][:2],16)
         E=int(dct['st'][2:],16)
-        assert E==0, "IFI SMX25: error from ST command. error = %d"%E
-        if (B1 & 1<<5) and (freq >= 250e6):
+        #assert E==0, "IFI SMX25: error from ST command. error = %d"%E
+        return B1, E
+        
+    def SetFreq(self, freq):
+        AMPLIFIER.SetFreq(self,freq)
+        B1,E=self._getstat()
+        #use ~B1 because active is LOW
+        # bit 5 is Band 1 active
+        # bit 6 is Band 2 active
+        if (~B1 & 1<<5) and (freq >= 250e6):
             self.write('B2')
-        elif (B1 & 1<<6) and (freq < 250e6):
+            while(~B1 & 1<<5 or E):
+                time.sleep(.2)
+                B1, E = self._getstat()
+        elif (~B1 & 1<<6) and (freq < 250e6):
             self.write('B1')
+            while(~B1 & 1<<6 or E):
+                time.sleep(.2)
+                B1, E = self._getstat()
         return self.error, freq
 
 def main():
     import sys
     import StringIO
+    import time
+    
     from mpy.tools.util import format_block
     import scuq
 
@@ -86,7 +102,10 @@ def main():
     amp=SMX25()
     err=amp.Init(ini)
     ctx=scuq.ucomponents.Context()
-    for freq in (80e6,500e6,1e9):
+    while(True):
+        freq=float(raw_input("Freq / Hz: "))
+        if freq < 0:
+            break
         amp.SetFreq(freq)
         err, uq = amp.GetData(what='S21')
         val, unc, unit=ctx.value_uncertainty_unit(uq)
