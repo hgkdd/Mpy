8219ed88eb629f81acf5029056a54f383b71ba8b
Stephan <devnull@localhost>
2009-06-11 13:00:48 +0200

    Neuer Treiber für SigGen R&S SMB100A

---
+++ mpy/device/sg_rs_smb100a.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+#
+import sys
+import StringIO
+from scuq import *
+from signalgenerator import SIGNALGENERATOR
+#
+#
+# Für den Signalgnerator SMB100A mit dem Softwarepaket SMB-B106 zur Verwendung bis 6GHz wird die Klasse 'SMB100A' definiert.
+# Diese greift auf die Unterklasse SIGNALGENERATOR (signalgenerator.py) und darüber auf die Unterklasse DRIVER (driver.py) zu.
+#
+class SMB100A(SIGNALGENERATOR):
+    def __init__(self):
+        SIGNALGENERATOR.__init__(self)
+        self._internal_unit='dBm'
+        #
+        # Im Wörterbuch '._cmds' werden die Befehle zum Steuern des speziellen Signalgenerators definiert, z.B. SetFreq() zum Setzen
+        # der Frequenz. Diese können in der Dokumentation des entsprechenden Signalgenerators nachgeschlagen werden.
+        # In der Unterklasse SIGNALGENERATOR wurden bereits Methoden zur Ansteuerung eines allgemeinen Signalgenerators definiert,
+        # welche die Steuerbefehle aus dem hier definierten '.cmds' Wörterbuch abrufen.
+        # Das Wörterbuch enthält für jeden Eintrag ein Schlüsselwort mit dem allgemeinen Befehl als String, z.B. SetFreq(). Diesem
+        # Schlüsselwort wird eine Liste zugeordnet, wobei jeder Listeneintrag ein Tupel ist und jeder Tupel einen Befehl und eine Vorlage
+        # für die darauffolgende Antwort des Signalgenerators enthaelt.
+        #
+        self._cmds={'Init':     [('*RST', None),
+                                 ('OUTP:STAT OFF', None)],
+                    'Quit':     [('OUTP:STAT OFF', None)],
+                    'RFOn':     [('OUTP:STAT ON', None)],
+                    'RFOff':    [('OUTP:STAT OFF', None)],
+                    'AMOn':     [('SOUR:AM:STAT ON', None)],
+                    'AMOff':    [('SOUR:AM:STAT OFF', None)],
+                    'PMOn':     [('SOUR:PM:STAT ON', None)],
+                    'PMOff':    [('SOUR:PM:STAT OFF', None)],                
+                    'SetFreq':  [("'SOUR:FREQ:CW %fHz'%freq", None)],
+                    'GetFreq':  [( 'SOUR:FREQ:CW?', r'(?P<freq>%s)'%self._FP)],
+                    'SetLevel': [("'SOUR:POW:LEVEL:IMM:AMPL %f'%self.convert.scuq2c(unit, self._internal_unit, float(level))[0]", None)],
+                    'GetLevel': [( 'SOUR:POW:LEVEL:IMM:AMPL?', r'(?P<level>%s)'%(self._FP))],
+                    'ConfAM':   [("'SOUR:AM:SOUR %s'%source", None), 
+                                 ( 'SOUR:AM:SOUR?', 'SOURCE (?P<source>\S+)'),
+                                 ("'SOUR:AM:DEPT %d '%(int(depth*100))", None), # Vorlage enthielt '%d %%' !!!???
+                                 ( 'SOUR:AM:DEPT?', 'DEPTH (?P<depth>\d+)'),
+                                 ("'SOUR:LFO:FREQ %s HZ'%freq", None),
+                                 ( 'SOUR:LFO:FREQ?', 'FREQ (?P<freq>%s) HZ'%self._FP),
+                                 ("'SOUR:LFO:SHAP %s'%(waveform)", None), # waveform --> SINE | SQUare
+                                 ( 'SOUR:LFO:SHAP?', 'WFRM (?P<waveform>\S+)')],
+                                 #("'LF:OUT %s'%(LFOut)", None),
+                                 #( 'LF:OUT?', 'LF (?P<LFOut>\S+)')],
+                    'GetDescription': [('*IDN?', r'(?P<IDN>.*)')]}
+        # 
+        #
+    def Init(self, ini=None, channel=None):
+        if channel is None:
+            channel=1
+        self.error=SIGNALGENERATOR.Init(self, ini, channel)
+        sec='channel_%d'%channel
+        try:
+            self.levelunit=self.conf[sec]['unit']
+        except KeyError:
+            self.levelunit=self._internal_unit
+        #   
+        # In der Methode 'main()' wird das Objekt sg der Klasse SMB100A definiert. Die Befehlsliste (dictionary) 'sg._cmds' der
+        # Klasse SMB100A wird mit einem Eintag namens 'Preset' erweitert und bekommt als Wert zunächst eine leere Liste zugewiesen.
+        # Als Wert wurde eine Liste gewählt, da zur Initilisierung mehrere Befehle notwendig sein können. Jedem Listeneintrag bzw.
+        # Initialisierungseschritt muss ein Tupel bestehend aus dem Befehl und der Auswertung der Signalgeneratorantwort zugewiesen
+        # werden. Zur Auswahl der notwendigen Initialisierungsschritte wird zunächst die Liste 'presets' definiert. Dabei handelt
+        # es sich um eine Art Tabelle mit drei Spalten, welche die möglichen Initialisierungsschritte und falls vorhanden zugehörigen
+        # Optionen inhaltet. 
+        #
+        self._cmds['Preset']=[]
+        presets=[('attmode',
+                      [('0','auto'),               ('1','fixed')],
+                      [('OUTP:AMOD AUTO', None),('OUTP:AMOD FIX', None)]),
+                 #('attenuation',
+                 #     None,
+                 #     ("'OUTP:ATT %f dB'%self.convert.c2c(self.levelunit, self._internal_unit, float(v))", None)),
+                 ('leveloffset',
+                      None,
+                      ("'SOUR:POW:LEV:IMM:OFFS %f'%self.convert.c2c(self.levelunit, self._internal_unit, float(v))", None)),
+                 ('levellimit',
+                      None,
+                      ("'SOUR:POW:LIM:AMPL %f'%self.convert.c2c(self.levelunit, self._internal_unit, float(v))", None)),
+                 ('level',
+                      None,
+                      ("'SOUR:POW:LEVEL:IMM:AMPL %f'%self.convert.c2c(self.levelunit, self._internal_unit, float(v))", None)),
+                 ('outputstate',
+                      [('1','on')],
+                      [('OUTP:STAT ON', None)])]
+        #
+        # Die zur Initialisierung des Signalgenerators notwendigen Schritte werden durch zeilenweise Betrachtung der Liste 'presets'
+        # herausgefiltert und in die Befehlsliste (dictionary) 'self._cmds' übertragen und stehen damit stehen auch in 'sg._cmds' zur
+        # Verfügung.
+        # Die Klassenvariable '.conf' (dictionary) wurde in der (Unter-)Klasse DRIVER definert.
+        # -> If / else Anweisung zur Behandlung von Initialisierungsschritten ohne Optionen (if) und mit Optionen (else).
+        # -> Bei Initialisierungsschritten mit Optionen erfolg die Auswahl der notwendigen Option über...(???)
+        # 
+        for k, vals, actions in presets:
+            print k, vals, actions
+            try:
+                v=self.conf[sec][k] 
+                if (vals is None):  
+                    self._cmds['Preset'].append((eval(actions[0]),actions[1]))
+                else:
+                    for idx,vi in enumerate(vals):
+                        if v.lower() in vi:
+                            self._cmds['Preset'].append(actions[idx])
+            except KeyError:
+                pass
+        #
+        # Initialisierung des Signalgenerators über die Methode '._do_cmds' der Klasse DRIVER (driver.py)
+        #
+        dct=self._do_cmds('Preset', locals())
+        self._update(dct)                
+        return self.error  
+#
+# Die Funktion main() wird nur zum Test des Treibers verwendet!
+#
+def main():
+    from mpy.tools.util import format_block
+    #
+    # Wird für den Test des Treibers keine ini-Datei über die Kommnadoweile eingegebnen, dann muss eine virtuelle Standard-ini-Datei erzeugt
+    # werden. Dazu wird der hinterlegte ini-Block mit Hilfe der Methode 'format_block' formatiert und der Ergebnis-String mit Hilfe des Modules
+    # 'StringIO' in eine virtuelle Datei umgewandelt.
+    #
+    try:
+        ini=sys.argv[1]
+    except IndexError:
+        ini=format_block("""
+                        [DESCRIPTION]
+                        description: 'SMB100A'
+                        type:        'SIGNALGENERATOR'
+                        vendor:      'Rohde&Schwarz'
+                        serialnr:
+                        deviceid:
+                        driver:
+
+                        [Init_Value]
+                        fstart: 100e6
+                        fstop: 6e9
+                        fstep: 1
+                        gpib: 15
+                        virtual: 1
+
+                        [Channel_1]
+                        name: RFOut
+                        level: -100
+                        unit: 'dBm'
+                        outpoutstate: 0
+                        """)
+        ini=StringIO.StringIO(ini)
+    #
+    # Zum Test des Treibers werden sogenannte Konsistenzabfragen ('assert' Bedingungen) verwendet, welche einen 'AssertationError' liefern,
+    # falls die Bedingung 'false' ist. Zuvor wird eine Testfrequenz und ein Level festgelegt, ein Objekt der Klasse SMB100A erzeugt und der
+    # Signalgenerator initialisiert.
+    #
+    lv=quantities.Quantity(si.WATT, 1e-4)
+    fr=300e6
+    sg=SMB100A()
+    err=sg.Init(ini)
+    assert err==0, 'Init() fails with error %d'%(err)
+    err,freq=sg.SetFreq(fr)
+    assert err==0, 'SetFreq() fails with error %d'%(err)
+    assert freq==fr, 'SetFreq() returns freq=%e instead of %e'%(freq, fr)
+    err, _ =sg.RFOn()
+    assert err==0, 'RFOn() fails with error %d'%(err)
+    err,level=sg.SetLevel(lv)
+    assert err==0, 'SetLevel() fails with error %d'%(err)
+    assert level==lv, 'SetLevel() returns level=%s instead of %s'%(level, lv)
+    err=sg.Quit()
+    assert err==0, 'Quit() fails with error %d'%(err)
+#
+#          
+#  ------------ Hauptprogramm ---------------------------
+#
+# Die Treiberdatei selbst und damit das Hauptprogramm wird nur gestartet, um den Treibercode zu testen. In diesem Fall springt
+# das Programm direkt in die Funktion 'main()'. Bei der späteren Verwendung des Treibers wird nur die Klasse 'SMB100A' und deren
+# Methoden importiert.
+#
+if __name__ == '__main__':
+    main()
+
+
+
+
