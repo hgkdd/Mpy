b82ca99d8e41c0204e6fb50b7555004b411db259
Hans Georg Krauthäuser <hgk@ieee.org>
2009-06-15 14:01:07 +0200

    seperte modules for kbhit and getch functions

---
+++ mpy/tools/kbhit.py
@@ -0,0 +1,25 @@
+try:
+    import msvcrt
+    kbhit = msvcrt.kbhit
+except ImportError:
+    import select, sys
+    def _KBHit_Unix():
+        """kbhit() - returns 1 if a key is ready, 0 othewise.
+           kbhit always returns immediately.
+        """
+        (read_ready, write_ready, except_ready) = \
+            select.select( [sys.stdin], [], [], 0.0)
+        if read_ready:
+            return 1
+        else:
+            return 0
+    kbhit = _KBHit_Unix
+
+
+if __name__ == "__main__":
+    while True:
+        ready = kbhit()
+        if ready:
+            break
+        else:
+            print '.',
