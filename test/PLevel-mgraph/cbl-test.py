91d69fad5dfa47b010157a4263d4366595d20037
Hans Georg Krauthäuser <hgk@ieee.org>
2010-10-19 14:10:33 +0200

    PLevelTest started at GTEM

---
+++ test/PLevel-mgraph/cbl-test.py
@@ -0,0 +1,12 @@
+from mpy.device.device import Cable
+
+ini="ini/amp1out-gtem.ini"
+
+cbl=Cable()
+cbl.Init(ini)
+
+for freq in [100e6, 1e9]:
+    cbl.SetFreq(freq)
+    err,dat= cbl.GetData('S21')
+    print freq, abs(dat)
+cbl.Quit()
