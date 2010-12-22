debugging of AmplifierTest: error with compression point calculation fixed

mpy/env/univers/AmplifierTest.py
@@ -3,13 +3,15 @@ import sys
 import re
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy.optimize import fminbound
+from scipy.optimize import fminbound, fsolve
 
 from scuq import quantities,si,units
 from mpy.env.Measure import Measure
 from mpy.tools.mgraph import MGraph
 from mpy.tools.aunits import POWERRATIO
 from mpy.tools import util
+
+extrap1d=util.extrap1d
 
 def dBm2W (v):
     return 10**(v*0.1)*0.001
@@ -340,8 +342,8 @@ Quit: quit measurement.
         r=re.compile(r'[(Quantity)\(, \)]*')
         idx=0
         while True:
-            anyprocessed=False
-            for f in freqs:
+            allprocessed=False
+            for f in sorted(freqs):
                 pinlst=[]
                 poutlst=[]
                 pin=rd['amp_pin'][f]
@@ -363,13 +365,16 @@ Quit: quit measurement.
                 if pinlst and len(pinlst)==len(poutlst):
                     #process list
                     gain, offset, pinc1, poutc1, pinc3, poutc3 = self._get_gain_compression(pinlst, poutlst, small_signal_factor=small_signal_factor)
+                    #dbgain=10*np.log10(gain.get_expectation_value_as_float())
+                    #dbpinc1, dbpoutc1, dbpinc3, dbpoutc3 = [10*np.log10(x.get_expectation_value_as_float()*1000) for x in (pinc1, poutc1, pinc3, poutc3)]
+                    #print f, dbgain, dbpinc1, dbpoutc1, dbpinc3, dbpoutc3, dbgain-(dbpoutc1-dbpinc1), dbgain-(dbpoutc3-dbpinc3) 
                     pdg[f].append(gain)
                     pdic1[f].append(pinc1)
                     pdic3[f].append(pinc3)
                     pdoc1[f].append(poutc1)
                     pdoc3[f].append(poutc3)
-                    anyprocessed=True
-            if not anyprocessed:
+                    allprocessed=True
+            if not allprocessed:
                 del pdg[f]
                 del pdic1[f]
                 del pdic3[f]
@@ -392,11 +397,22 @@ Quit: quit measurement.
         pout_ss = pout_vals[:len(pin_ss)]
         gain, offset = np.polyfit(pin_ss, pout_ss, 1)
         ideal = lambda pi: offset+gain*pi  # linear
-        orig = interp1d(pin_vals, pout_vals)
+        orig = extrap1d(interp1d(pin_vals, pout_vals))
+        # c1func = lambda pi: abs(ideal(pi)-orig(pi)*1.259)   # 1 dB
+        # c3func = lambda pi: abs(ideal(pi)-orig(pi)*1.995)   # 3 dB
+        # pinc1 = fminbound(c1func, 0.5*pin_vals[-1], pin_vals[-1])[0]
+        # pinc3 = fminbound(c3func, pinc1, pin_vals[-1])[0]
         c1func = lambda pi: abs(ideal(pi)-orig(pi)*1.259)   # 1 dB
         c3func = lambda pi: abs(ideal(pi)-orig(pi)*1.995)   # 3 dB
-        pinc1 = fminbound(c1func, 0.5*pin_vals[-1], pin_vals[-1])[0]
-        pinc3 = fminbound(c3func, pinc1, pin_vals[-1])[0]
+        lower=pin_ss[-1]
+        for _ in range(100):
+            pinc1 = fminbound(c1func, lower, 10*pin_vals[-1], xtol=1e-7, maxfun=1000)[0]
+            if c1func(pinc1) > 0.01:
+                #print pinc1, c1func(pinc1) 
+                lower=pinc1
+            else:
+                break
+        pinc3 = fminbound(c3func, pinc1, 10*pin_vals[-1], xtol=1e-7, maxfun=1000)[0]
         poutc1=float(orig(pinc1))
         poutc3=float(orig(pinc3))
         # make quantities

test/PLevel-mgraph/AT-test-AR25S1G4.py
@@ -1,5 +1,5 @@
 import cPickle as pickle
-from numpy import linspace,concatenate
+from numpy import linspace,concatenate, log10
 from scuq.quantities import Quantity
 from scuq.si import WATT
 import mpy.env.univers.AmplifierTest
@@ -7,12 +7,14 @@ import mpy.env.univers.AmplifierTest
 def dBm2W (v):
     return 10**(v*0.1)*0.001
 def W2dBm (v):
-    return 10*np.log10(v*1000)
+    return 10*log10(v*1000)
 
 description="AR-25S1G4"
+MpyDIRS=['\\MpyConfig\\LargeGTEM', '.']
+
     
-if False:
-    dot='amplifier-test.dot'
+if True:
+    dot='amplifier-test-sw800e6.dot'
     # keys: names in program, values: names in graph
     names={'sg': 'sg',
            'amp_in': 'amp_in',
@@ -21,7 +23,7 @@ if False:
            'pm_bwd': 'pm2',
            'output': 'gtem'}
 
-    AT = mpy.env.univers.AmplifierTest.AmplifierTest()
+    AT = mpy.env.univers.AmplifierTest.AmplifierTest(SearchPaths=MpyDIRS)
     AT.set_logfile('%s.log'%description)
 
     freqs=linspace(800e6, 4.2e9, 50)
@@ -30,9 +32,9 @@ if False:
                dotfile=dot,
                names=names,
                freqs=freqs,
-               levels=[Quantity(WATT, dBm2W(dBmval)) for dBmval in linspace(-30, 0, 31)])
-    pickle.dump (AT, file('%s.p'%description, 'wb'), 2)
+               levels=[Quantity(WATT, dBm2W(dBmval)) for dBmval in linspace(-30, 3, 34)])
+    pickle.dump (AT, file('%s-new.p'%description, 'wb'), 2)
 else:
-    AT=pickle.load (file('%s.p'%description, 'rb'))
+    AT=pickle.load (file('%s-new.p'%description, 'rb'))
     AT.GetGainAndCompression(description=description)
-    pickle.dump (AT, file('%s-processed.p'%description, 'wb'), 2)
+    pickle.dump (AT, file('%s-new-processed.p'%description, 'wb'), 2)

test/PLevel-mgraph/AT-test-IFISMX25.py
@@ -1,5 +1,5 @@
 import cPickle as pickle
-from numpy import linspace,concatenate
+from numpy import linspace,concatenate, log10
 from scuq.quantities import Quantity
 from scuq.si import WATT
 import mpy.env.univers.AmplifierTest
@@ -8,14 +8,14 @@ from mpy.tools.util import locate
 def dBm2W (v):
     return 10**(v*0.1)*0.001
 def W2dBm (v):
-    return 10*np.log10(v*1000)
+    return 10*log10(v*1000)
 
 description="IFI SMX25"
 MpyDIRS=['\\MpyConfig\\LargeGTEM', '.']
 
 
-if True:
-    dot='gtem-immunity.dot'
+if False:
+    dot='amplifier-test-sw1e9.dot'
     #print dot
     # keys: names in program, values: names in graph
     names={'sg': 'sg',
@@ -31,14 +31,14 @@ if True:
     freqb1=linspace(10e3, 200e6, 20)
     freqb2=linspace(200e6, 1e9 , 20)
     freqs = concatenate((freqb1,freqb2))
-    levels=[Quantity(WATT, dBm2W(dBmval)) for dBmval in linspace(-5, 0, 6)]
+    levels=[Quantity(WATT, dBm2W(dBmval)) for dBmval in linspace(-30, 3, 34)]
     AT.Measure(description=description,
                dotfile=dot,
                names=names,
                freqs=freqs,
                levels=levels, virtual=False)
-    pickle.dump (AT, file('%s.p'%description, 'wb'), 2)
+    pickle.dump (AT, file('%s-new.p'%description, 'wb'), 2)
 else:
-    AT=pickle.load (file('%s.p'%description, 'rb'))
+    AT=pickle.load (file('%s-new.p'%description, 'rb'))
     AT.GetGainAndCompression(description=description)
-    pickle.dump (AT, file('%s-processed.p'%description, 'wb'), 2)
+    pickle.dump (AT, file('%s-new-processed.p'%description, 'wb'), 2)