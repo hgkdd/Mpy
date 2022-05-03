d1359864bf9a72c03e9bec2d2a30833bf9d6e3ae
Hans Georg Krauthäuser <hgk@ieee.org>
2022-05-03 13:50:27 +0200

    adjustments fpr python3 packaging scheme

--- setup.py
+++ setup.py
@@ -1,26 +1,18 @@
-from ez_setup import use_setuptools
-use_setuptools()
-import os
-from setuptools import setup, find_packages
-import version
+import setuptools
 
-ver=''
-version.remember_version(ver)
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(name='mpy',
-      version=version.get_version(),
-      author="Hans Georg Krauthaeuser",
-      author_email="hgk@ieee.org",
-      description='instrument control framework',
-      license='All rights reserved',
-      url='http://tu-dresden.de/et/tet',
-      install_requires=[#'numpy',
-                        #'scipy',
-                        #'pydot',
-                        #'pyvisa',
-                        #'ply'
-                        ],
-      packages=find_packages(),
-      #package_dir = {'':'mpy'},
-      #data_files=[('mpy', ['mpy/LICENSE'])],
-      )
+
+setuptools.setup(
+    name='mpy-hgkTUD',
+    author="Hans Georg Krauthaeuser",
+    author_email="hgk@ieee.org",
+    description='instrument control framework',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://tu-dresden.de/et/tet',
+    packages=setuptools.find_packages(),
+    #package_dir = {'':'mpy'},
+    #data_files=[('mpy', ['mpy/LICENSE'])],
+)
