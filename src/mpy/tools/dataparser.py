ac09a9059d58c353cebc9383b7946a499619be43
Hans Georg Krauthäuser <hgk@ieee.org>
2023-07-12 14:40:19 +0200

    Code Cleaning

--- src/mpy/tools/dataparser.py
+++ src/mpy/tools/dataparser.py
@@ -32,20 +32,20 @@ class UConv(object):
             "v":    (si.VOLT, _ident),
             "db":   (POWERRATIO, _dBfac(10)),
             "hz":   (si.HERTZ, _ident),
-            "khz":  (si.HERTZ, _mulfac(_ident,1e3)),
-            "mhz":  (si.HERTZ, _mulfac(_ident,1e6)),
-            "ghz":  (si.HERTZ, _mulfac(_ident,1e9)),
+            "khz":  (si.HERTZ, _mulfac(_ident, 1e3)),
+            "mhz":  (si.HERTZ, _mulfac(_ident, 1e6)),
+            "ghz":  (si.HERTZ, _mulfac(_ident, 1e9)),
             "v/m":  (EFIELD, _ident),
             "dbv/m": (EFIELD, _dBfac(20)),
             "m": (si.METER, _ident),
-            "cm": (si.METER, _mulfac(_ident,1e-2)),
-            "mm": (si.METER, _mulfac(_ident,1e-3)),
+            "cm": (si.METER, _mulfac(_ident, 1e-2)),
+            "mm": (si.METER, _mulfac(_ident, 1e-3)),
             "deg": (si.RADIAN, _mulfac(_ident, math.pi/180.0)),
             "rad": (si.RADIAN, _ident),
             "steps": (units.ONE, _ident), 
             "db1/m": (EFIELD/si.VOLT, _dBfac(20)),
             "dbi": (POWERRATIO, _dBfac(10)),
-            "dbd": (POWERRATIO,  _mulfac(_dBfac(10),1.64)),   # 1.64: Directivity of a half wave dipole
+            "dbd": (POWERRATIO,  _mulfac(_dBfac(10), 1.64)),   # 1.64: Directivity of a half wave dipole
             "1/m": (EFIELD/si.VOLT, _ident),
             "a/m": (HFIELD, _ident),
             "dba/m": (HFIELD, _dBfac(20)),
@@ -55,8 +55,8 @@ class UConv(object):
             "dbs/m": (HFIELD/si.VOLT, _dBfac(20)),
             "amplituderatio": (AMPLITUDERATIO, _ident),
             "powerratio": (POWERRATIO, _ident),
-            "h": (si.HENRY,_ident),
-            "f": (si.FARAD,_ident)}
+            "h": (si.HENRY, _ident),
+            "f": (si.FARAD, _ident)}
 
 
 class DatFile(Parser):
@@ -116,8 +116,8 @@ class DatFile(Parser):
                          'COMMA',
                          'NEWLINE')
 
-    t_LBRACE =  r'\('
-    t_RBRACE =  r'\)'
+    t_LBRACE = r'\('
+    t_RBRACE = r'\)'
     t_LSBRACE = r'\['
     t_RSBRACE = r'\]'
     t_COMMA = r','
@@ -126,15 +126,15 @@ class DatFile(Parser):
 
     # A string containing ignored characters (spaces and tabs)
     
-    t_ignore  = ' \t:'
+    t_ignore = ' \t:'
 
     def __init__(self, **kw):
-        self.funit=None
-        self.fromunit=None
-        self.tounit=None
-        self.relerror=None
-        self.abserror=None
-        self.data={}
+        self.funit = None
+        self.fromunit = None
+        self.tounit = None
+        self.relerror = None
+        self.abserror = None
+        self.data = {}
         Parser.__init__(self, **kw)
         
 
@@ -145,7 +145,7 @@ class DatFile(Parser):
         try:
             t.value = float(t.value)    
         except ValueError:
-            print(("Line %d: Number %s is too large!" % (t.lineno,t.value)))
+            print("Line %d: Number %s is too large!" % (t.lineno, t.value))
             t.value = 0
         return t
 
@@ -167,113 +167,108 @@ class DatFile(Parser):
 
     # Error handling rule
     def t_error(self, t):
-        print(("Illegal character '%s'" % t.value[0]))
+        print("Illegal character '%s'" % t.value[0])
         t.lexer.skip(1)
 
     # Parser starts here
 
     def p_lines_line(self, p):
-        '''lines : line lines
-                 | line'''
-        p[0]=self.data
-
+        """lines : line lines
+                | line"""
+        p[0] = self.data
 
     def p_line_empty(self, p):
-        'line : NEWLINE'
+        r'line : NEWLINE'
         pass
 
     def p_line_funit(self, p):
-        'line : FUNIT idanycase NEWLINE'
-        #print p[2]
+        r'line : FUNIT idanycase NEWLINE'
+        # print(p[2])
         self.funit = p[2]
 
     def p_line_unit(self, p):
-        '''line : UNIT idanycase NEWLINE
-                 | UNIT idanycase idanycase NEWLINE'''
-        if len(p)==4:
-            self.fromunit=self.tounit=p[2]
-        elif len(p)==5:
-            self.fromunit=p[2]
-            self.tounit=p[3]
+        """line : UNIT idanycase NEWLINE
+                | UNIT idanycase idanycase NEWLINE"""
+        if len(p) == 4:
+            self.fromunit = self.tounit = p[2]
+        elif len(p) == 5:
+            self.fromunit = p[2]
+            self.tounit = p[3]
 
     def p_idanycase_id(self, p):
-        'idanycase : ID'
-        #print len(p)
-        p[0]=p[1].lower()
+        r'idanycase : ID'
+        # print(len(p))
+        p[0] = p[1].lower()
 
     def p_line_relerr(self, p):
-        'line : RELERROR FPNUMBER NEWLINE'
-        self.abserror=None
-        self.relerror=float(p[2])
+        r'line : RELERROR FPNUMBER NEWLINE'
+        self.abserror = None
+        self.relerror = float(p[2])
 
     def p_line_abserr(self, p):
-        'line : ABSERROR val NEWLINE'
-        self.relerror=None
-        self.abserror=p[2]
+        r'line : ABSERROR val NEWLINE'
+        self.relerror = None
+        self.abserror = p[2]
         
 
     def p_line_data1(self, p):
-        'line : FPNUMBER val val val NEWLINE'
-        freq=UConv.uconv[self.funit][1](p[1])
-        uq=self._makeuq(p[2],p[3],p[4], UConv.uconv[self.fromunit][0])
+        r'line : FPNUMBER val val val NEWLINE'
+        freq = UConv.uconv[self.funit][1](p[1])
+        uq = self._makeuq(p[2], p[3], p[4], UConv.uconv[self.fromunit][0])
         self.data.setdefault(freq, uq)
 
     def p_line_data2(self, p):
-        'line : FPNUMBER val NEWLINE'
-        freq=UConv.uconv[self.funit][1](p[1])
-        v=p[2]
-        s=0
+        r'line : FPNUMBER val NEWLINE'
+        freq = UConv.uconv[self.funit][1](p[1])
+        v = p[2]
+        s = 0
         if self.relerror:
-            s=v*self.relerror
+            s = v*self.relerror
         if self.abserror:
-            s=self.abserror
-        ui=ucomponents.UncertainInput(v,s)
-        uq=quantities.Quantity(UConv.uconv[self.fromunit][0], ui)
+            s = self.abserror
+        ui = ucomponents.UncertainInput(v, s)
+        uq = quantities.Quantity(UConv.uconv[self.fromunit][0], ui)
         self.data.setdefault(freq, uq)
 
-
     def p_val_number(self, p):
-        'val : number'
-        p[0]=p[1]
+        r'val : number'
+        p[0] = p[1]
 
     def p_number_fpnumber(self, p):
-        'number : FPNUMBER'
-        p1=UConv.uconv[self.fromunit][1](p[1])
-        p[0]=p1
+        r'number : FPNUMBER'
+        p1 = UConv.uconv[self.fromunit][1](p[1])
+        p[0] = p1
 
     def p_val_ri(self, p):
-        'val : LBRACE FPNUMBER COMMA FPNUMBER RBRACE'
-        p2=UConv.uconv[self.fromunit][1](p[2])
-        p4=UConv.uconv[self.fromunit][1](p[4])
-        p[0]=complex(p2,p4)
+        r'val : LBRACE FPNUMBER COMMA FPNUMBER RBRACE'
+        p2 = UConv.uconv[self.fromunit][1](p[2])
+        p4 = UConv.uconv[self.fromunit][1](p[4])
+        p[0] = complex(p2, p4)
 
     def p_val_ma(self, p):
-        'val : LSBRACE FPNUMBER COMMA FPNUMBER RSBRACE'
-        m=UConv.uconv[self.fromunit][1](p[2])
-        ang=float(p[4])*math.pi/180.        
-        p[0]=complex(m*math.cos(ang),m*math.sin(ang))
-
-
+        r'val : LSBRACE FPNUMBER COMMA FPNUMBER RSBRACE'
+        m = UConv.uconv[self.fromunit][1](p[2])
+        ang = float(p[4])*math.pi/180.
+        p[0] = complex(m*math.cos(ang), m*math.sin(ang))
 
     # Error rule for syntax errors
     def p_error(self, p):
         print("Syntax error in input!")
-
     def _makeuq(self, a, b, c, unit):
         def cplx_cmp(a):
             try:
                 # length(a) * sgn(a.real)
-                ma=abs(a)*a.real/abs(a.real)
+                ma = abs(a)*a.real/abs(a.real)
             except AttributeError:
-                ma=a
+                ma = a
             # try:
             #     mb=abs(b)*b.real/abs(b.real)
             # except AttributeError:
             #     mb=b
             return ma   # cmp(ma,mb)
-        l,v,u=sorted((a,b,c), key=cplx_cmp)
-        delta=(u-l)*0.5
-        ui=ucomponents.UncertainInput(v,delta)
+        l, v, u = sorted((a, b, c), key=cplx_cmp)
+        delta = (u-l)*0.5
+        ui = ucomponents.UncertainInput(v, delta)
         return quantities.Quantity(unit, ui)
 
 
@@ -282,11 +277,11 @@ if __name__ == '__main__':
     import io
     from mpy.tools.util import format_block
 
-    name=None
-    if len(sys.argv)>1:
-        name=sys.argv[1]
+    name = None
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
     else:
-        name=io.StringIO(format_block("""
+        name = io.StringIO(format_block("""
                                             FUNIT: Hz
                                             UNIT: powerratio
                                             ABSERROR: [0.1, 1]
@@ -302,10 +297,10 @@ if __name__ == '__main__':
                                             90 [0.2, 50]
                                             """))
 
-    DF=DatFile(filename=name)
-    result=DF.run()
-    ctx=ucomponents.Context()
+    DF = DatFile(filename=name)
+    result = DF.run()
+    ctx = ucomponents.Context()
     for f in sorted(result):
-        uq=result[f]
-        val,err,unit=ctx.value_uncertainty_unit(uq)
+        uq = result[f]
+        val, err, unit = ctx.value_uncertainty_unit(uq)
         print(f, uq, val, err, unit)
