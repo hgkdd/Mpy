2d2bcab0741b6d510478cfc4436d05e208f2b20b
hgkrauth <hgk@ieee.org>
2025-11-05 15:26:49 +0100

    add class for emc receiver

---
+++ src/mpylab/device/receiver.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+This is the :mod:`mpylab.device.receiver` module.
+
+   :author: Hans Georg Krauthäuser (main author)
+
+   :license: GPLv3 or higher
+"""
+
+
+from scuq import *
+
+from mpylab.device.driver import DRIVER
+from mpylab.tools.configuration import strbool
+
+
+class RECEIVER(DRIVER):
+    """
+    Child class for all py-drivers for EMC receivers.
+    
+    The parent class is :class:`mpylab.device.driver.DRIVER`.
+    
+    The configuration template for this device class is::
+    
+        conftmpl={'description': 
+                     {'description': str,
+                      'type': str,
+                      'vendor': str,
+                      'serialnr': str,
+                      'deviceid': str,
+                      'driver': str},
+                    'init_value':
+                        {'fstart': float,
+                         'fstop': float,
+                         'fstep': float,
+                         'visa': str,
+                         'virtual': strbool,
+                         'nr_of_channels': int},
+                    'channel_%d':
+                        {'name': str,
+                         'detector': str,
+                         'attenuation': str,
+                         'meas_time': float,
+                         'min_attenuation': int,
+                         'unit': str,
+                         'preamplifier': str}}
+       
+    The meaning is:
+        
+    - Section *description*
+        - description: string describing the instrument
+        - type: string with the instrument type (here: POWERMETER)
+        - vendor: string ddescribing the vendor/manufactor
+        - serialnr: string with a unique identification
+        - deviceid: string with an internal id
+        - driver: filename of the instrument driver (.py, .pyc, .pyd, .dll)
+    - Section *init_value*
+        - *fstart*: lowest possible frequency in Hz of the device
+        - *fstop*: highest possible frequency in Hz of the device
+        - *fstep*: smallest frequency step in Hz of the device
+        - *visa*: VISA identifier
+        - *virtual*: 0, false or 1, true. Virtual device are usefull for testing and debugging.
+        - *nr_of_channels*: indicates how many channel sections follow
+    - Section *channel_%d* (*%d* may be 1, 2, ...)
+        - *name*: a string identifying the channel.
+        - *detector*: detector used fpr this channel
+        - *unit*: a string containing the unit of the returned power/voltage readings.
+          However, :mod:`scuq` will ignore dB-settings, and the returned power/voltage will contain
+          the unit anyway.
+        - *meas_time*: the measuring time for that channel
+        - *attenuation*: value of the attenuation, may be *auto*
+        - *min_attenuation*: value of the minimum attenuation
+        - *detector*: 'PEAK', 'AVERAGE', 'QPEAK' or a comma separated list
+        - *preamplifier*: 'on' or 'off'
+    """
+
+    conftmpl = {'description':
+                    {'description': str,
+                     'type': str,
+                     'vendor': str,
+                     'serialnr': str,
+                     'deviceid': str,
+                     'driver': str},
+                'init_value':
+                    {'fstart': float,
+                     'fstop': float,
+                     'fstep': float,
+                     'visa': str,
+                     'virtual': strbool,
+                     'nr_of_channels': int},
+                'channel_%d':
+                    {'name': str,
+                     'detector': str,
+                     'attenuation': str,
+                     'min_attenuation': int,
+                     'meas_time': float,
+                     'rbw': str,
+                     'unit': str,
+                     'preamplifier': str}}
+
+    _FP = r'[-+]?(\d+(\.\d*)?|\d*\.\d+)([eE][-+]?\d+)?'
+
+    def __init__(self):
+        DRIVER.__init__(self)
+        self._cmds = {'SetFreq': [("f'FREQUENCY {freq} HZ'", None)],
+                      'GetFreq': [('FREQUENCY?', r'FREQUENCY (?P<freq>%s)' % self._FP)],
+                      'GetData': [('LEVEL?', r'LEVEL (?P<lev>%s)' % self._FP)],
+                      'GetDataNB': [('LEVEL:LASTVALUE?', r'LEVEL:LASTVALUE (?P<lev>%s)' % self._FP)],
+                      'Trigger': [('*TRG', None)],
+                      'SetAttenuation': [("f'ATTENUATION {attenuation} DB'", None)],
+                      'GetAttenuation': [('ATTENUATION?', r'ATTENUATION (?P<attenuation>%s)' % self._FP)],
+                      'SetMinAttenuation': [("f'MIN:ATTENUATION {min_attenuation} DB'", None)],
+                      'GetMinAttenuation': [('MIN:ATTENUATION?', r'MIN:ATTENUATION (?P<min_attenuation>%s)' % self._FP)],
+                      'SetMeasTime': [("f'MEASUREMENT:TIME {meas_time} s'", None)],
+                      'GetMeasTime': [('MEASUREMENT:TIME?', r'MEASUREMENT:TIME (?P<meas_time>%s)' % self._FP)],
+                      'SetDetector': [("f'DETECTOR {detector}'", None)],
+                      'GetDetector': [('DETECTOR?', r'DETECTOR (?P<detector>.*)')],
+                      'SetPreamplifier': [("f'PREAMPLIFIER {preamplifier}'", None)],
+                      'GetPreamplifier': [('PREAMPLIFIER?', r'PREAMPLIFIER (?P<preamplifier>.*)')],
+                      'SetResolutionBandwidth': [("f'BANDWIDTH:IF {rbw} HZ'", None)],
+                      'GetResolutionBandwidth': [('BANDWIDTH:IF?', r'BANDWIDTH:IF (?P<bbw>%s)' % self._FP)],
+                      'Quit': [('*CLS', None)],
+                      'GetDescription': [('*IDN?', r'(?P<IDN>.*)')]}
+        self.freq = None
+        self.power = None
+        self.unit = None
+        self.channel = None
+        self._internal_unit = 'dBuV'
+
+    def SetFreq(self, freq):
+        """
+        Set the frequency to *freq* (in Hz).
+        
+        After setting, the freq is read back from the device.
+        
+        ``(self.error, self.freq)`` is returned.
+        """
+        self.error = 0
+        # print freq
+        dct = self._do_cmds('SetFreq', locals())
+        self._update(dct)
+        dct = self._do_cmds('GetFreq', locals())
+        self._update(dct)
+        if self.error == 0:
+            if not dct:
+                self.freq = freq
+            else:
+                self.freq = float(self.freq)
+            # print self.freq
+        return self.error, self.freq
+
+    def Trigger(self):
+        """
+        Trigger a single measurement.
+        
+        ``self.error`` is returned.
+        """
+        self.error = 0
+        dct = self._do_cmds('Trigger', locals())
+        self._update(dct)
+        # if self.error == 0:
+        #    print "Device triggered."
+        return self.error
+
+
+    def GetData(self):
+        """
+        Read a power measurement from the instrument.
+        
+        ``(self.error, obj)`` is returned where ``obj`` is a instance of 
+        :class:`scuq.quantities.Quantity`.
+        """
+        self.error = 0
+        dct = self._do_cmds('GetData', locals())
+        self._update(dct)
+
+        if self.error == 0 and self.power:
+            self.update_internal_unit()
+            self.power = float(self.power)
+            try:
+                obj = quantities.Quantity(eval(self._internal_unit),
+                                          ucomponents.UncertainInput(self.power, 0))
+            except (AssertionError, NameError):
+                self.power, self.unit = self.convert.c2scuq(self._internal_unit, float(self.power))
+                obj = quantities.Quantity(self.unit,
+                                          ucomponents.UncertainInput(self.power, 0))
+        else:
+            obj = None
+        return self.error, obj
+
+    def GetDataNB(self, retrigger):
+        """
+        Non-blocking version of :meth:`GetData`.
+        
+        If implemented, this function will return ``(-1, None)`` until the answer from the device is available.
+        Then, it will return ``self.error, obj)``.
+        
+        If *retrigger* is ``True`` or ``'on'``, the device will be triggered for a new measurment after the measurement has been 
+        red.
+        
+        If not implemented, the method will return :meth:`GetData`.
+        """
+        self.error, obj = self.GetData()
+        if retrigger in (True, 'ON', 'On', 'on'):
+            self.Trigger()
+        return self.error, obj
+
+    def update_internal_unit(self, ch=None, unit='DBUV'):
+        """
+        Selects the output unit for the measured power values.
+       
+        Parameters:
+            
+        - *ch*: an integer specifiing the channel number of multi channel devices. Numbering is starting with 1.
+        - *unit*: an string specifiing the unit for the measured data.
+
+        The table shows the posibilities::
+        
+              Unit        SCPI notation
+              Watt           W
+              dB             DB
+              dBm            DBM
+              dBuV           DBUV
+
+        """
+        unit = unit
+        channel = ch
+        if not channel:
+            channel = self.channel
+        dct = self._do_cmds('Unit', locals())
+        self._internal_unit = unit
+
+
+if __name__ == '__main__':
+    import sys, io
+    from mpylab.tools.util import format_block
+    try:
+        ini = sys.argv[1]
+    except IndexError:
+        ini = format_block("""
+                        [DESCRIPTION]
+                        description: 'R&S ESHS30'
+                        type:        'RECEIVER'
+                        vendor:      'Rohde&Schwarz'
+                        serialnr:
+                        deviceid:
+                        driver: rec_rs_ESHS30.py
+
+                        [Init_Value]
+                        fstart: 9e6
+                        fstop: 30e6
+                        fstep: 1
+                        visa: GPIB0::17::INSTR
+                        virtual: 0
+
+                        [Channel_1]
+                        name: RFin
+                        min_attenuation: 10
+                        meas_time: 0.05
+                        preamplifier: on
+                        unit: dBuV
+                        attenuation: auto
+                        rbw: auto
+                        detector: PEAK
+                        """)
+        # rbw: 3e6
+        ini = io.StringIO(ini)
+
+
+    d = RECEIVER()
+    d.Init(ininame=ini, channel=1)
+    if not ini:
+        d.SetVirtual(False)
+
+    err, des = d.GetDescription()
+    print(("Description: %s" % des))
+
+    for freq in [9e3, 100e3, 500e3, 1e6, 10e6, 30e6]:
+        print(("Set freq to %e Hz" % freq))
+        err, rfreq = d.SetFreq(freq)
+        if err == 0:
+            print(("Freq set to %e Hz" % rfreq))
+        else:
+            print("Error setting freq")
+
+    d.Quit()
