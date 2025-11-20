9f04a0c72863422de6e6a2bbc673b138c2ab9f59
hgkdd <75255150+hgkdd@users.noreply.github.com>
2025-11-20 16:11:40 +0100

    cosmetic change in init

--- src/mpylab/device/nport.py
+++ src/mpylab/device/nport.py
@@ -37,8 +37,8 @@ class NPORT(DRIVER):
         self.conf = {'init_value': {'virtual': False}}
         self.data = {}
 
-    def Init(self, ini, channel=None):
-        DRIVER.Init(self, ini, channel=channel)
+    def Init(self, ini=None, channel=None):
+        super().Init(ini=ini, channel=channel)
         # self.error=0
         # self.Configuration=Configuration(ini, self.conftmpl)
         # print self.Configuration.conf
