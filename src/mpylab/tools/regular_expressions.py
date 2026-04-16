af8fb65fff17bcea2126f8c2f876928f55a1e823
hgkdd <75255150+hgkdd@users.noreply.github.com>
2026-04-16 12:48:28 +0200

    add re for inline file blocks (no longer use eval)

--- src/mpylab/tools/regular_expressions.py
+++ src/mpylab/tools/regular_expressions.py
@@ -6,7 +6,18 @@ This is the :mod:`mpylab.tools.regular_expressions` module.
 
    :license: GPLv3 or higher
 """
+import re
 
 FP = r'[-+]?(\d+(\.\d*)?|\d*\.\d+)([eE][-+]?\d+)?'
 FP_anchor_start = r'^[-+]?(\d+(\.\d*)?|\d*\.\d+)([eE][-+]?\d+)?'
 FP_anchor_end = r'[-+]?(\d+(\.\d*)?|\d*\.\d+)([eE][-+]?\d+)?$'
+
+_INLINE_FILE_RE = re.compile(
+    r"""^io\.StringIO\(\s*format_block\(\s*([\"']{3})(.*)\1\s*\)\s*\)\s*$""",
+    re.DOTALL,
+)
+
+_INLINE_FILE_BLOCK_RE = re.compile(
+    r"""io\.StringIO\(\s*format_block\(\s*([\"']{3})(.*?)\1\s*\)\s*\)""",
+    re.DOTALL,
+)
